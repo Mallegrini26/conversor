df6b63d1e9b7ea390520e7367bdef619e03157e8
Mallegrini26 <allegrinimarcelo.@gmail.com>
2024-12-10 10:00:22 -0300

    cambio de estilos

--- java.js
+++ java.js
@@ -1,17 +1,4 @@
-/*
-document.getElementById('boton').addEventListener('click', function() {
-    const tasaDeCambio = 0.001; // Ejemplo de tasa de cambio fija (1 peso = 0.001 dólares)
-    const cantidadPesos = parseFloat(document.getElementById('num').value);
-    
-    if (isNaN(cantidadPesos)) {
-        alert('Por favor, ingresa una cantidad válida.');
-        return;
-    }
 
-    const cantidadDolares = cantidadPesos * tasaDeCambio;
-    document.getElementById('num1').value = cantidadDolares.toFixed(2);
-});
-*/
 function mostrar() {
     document.getElementById("fecha").innerHTML = Date();
 }
@@ -24,7 +11,6 @@ document.getElementById('boton').addEventListener('click', function() {
         alert('Por favor, ingresa una cantidad válida.');
         return;
     }
-
     let resultado;
     if (direccion === 'pesos-a-dolares') {
         resultado = cantidad * tasaDeCambio;
